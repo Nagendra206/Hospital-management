4dd92ccb8d1dbe725e6fece63d2064dbff3588b9
Nagendra206 <121850704+Nagendra206@users.noreply.github.com>
2023-07-27 18:20:14 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,11 @@
-var mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-//change the database with yours
-const uri = 'mongodb+srv://nagendra:CeVrL0lYPMJCX7CU@cluster0.8oaoyxf.mongodb.net/?retryWrites=true&w=majority';
-mongoose.connect('mongodb://nagendra:CeVrL0lYPMJCX7CU@cluster0.8oaoyxf.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB Atlas'))
+const uri = "mongodb+srv://nagendra:CeVrL0lYPMJCX7CU@cluster0.8oaoyxf.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+    // Perform any other initialization tasks here
+  })
   .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
